8e717f2abde0b9ec78682a1243d73b4ee3858d9f
Paul Thorsen <pkt1995@gmail.com>
2019-12-28 13:34:27 -0600

    Initial Commit

---
+++ Tasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tasks
+//
+//  Created by Paul Thorsen on 12/28/19.
+//  Copyright © 2019 Paul Thorsen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
